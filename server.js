7c68e93b2b28e15dc1273e0d6c3d90230d93c126
rishu2004-rs <kpuja0969@gmail.com>
2026-06-02 00:40:58 +0530

    fixed cors error

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use('/uploads', express.static('uploads'));
 
